6d0b8a99297c0264206f0704607bd8ded867aba3
Rafael Toledo <toledorafael304@gmail.com>
2022-09-11 12:35:06 -0300

    Date Select

---
+++ backend/dsmeta/src/main/java/com/devsuperior/dsmeta/repository/SaleRepository.java
@@ -0,0 +1,25 @@
+package com.devsuperior.dsmeta.repository;
+
+import com.devsuperior.dsmeta.entities.Sale;
+import java.time.LocalDate;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+public interface SaleRepository extends JpaRepository<Sale, Long> {
+
+    @Query(""
+            + "SELECT "
+            + "  obj "
+            + "FROM "
+            + "  Sale obj "
+            + "WHERE "
+            + "  obj.date "
+            + "BETWEEN "
+            + "  :min AND :max "
+            + "ORDER BY obj.amount DESC"
+            + "")
+    public Page<Sale> findSales(LocalDate min, LocalDate max, Pageable pageable);
+
+}
--- backend/dsmeta/src/main/resources/import.sql
+++ backend/dsmeta/src/main/resources/import.sql
@@ -1,11 +1,3 @@
-/* 
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Other/SQLTemplate.sql to edit this template
- */
-/**
- * Author:  rafaeltoledo
- * Created: 7 de set. de 2022
- */
 INSERT INTO tb_sales(seller_name,visited,deals,amount,date) VALUES ('Barry Allen',121,67,18196.0,'2022-06-16');
 INSERT INTO tb_sales(seller_name,visited,deals,amount,date) VALUES ('Logan',26,14,4255.0,'2022-06-14');
 INSERT INTO tb_sales(seller_name,visited,deals,amount,date) VALUES ('Padme',55,42,13249.0,'2022-06-14');
@@ -41,7 +33,7 @@ INSERT INTO tb_sales(seller_name,visited,deals,amount,date) VALUES ('Logan',32,1
 INSERT INTO tb_sales(seller_name,visited,deals,amount,date) VALUES ('Logan',64,33,8193.0,'2022-03-13');
 INSERT INTO tb_sales(seller_name,visited,deals,amount,date) VALUES ('Barry Allen',39,39,10557.0,'2022-03-05');
 INSERT INTO tb_sales(seller_name,visited,deals,amount,date) VALUES ('Barry Allen',158,84,21601.0,'2022-03-02');
-INSERT INTO tb_sales(seller_name,visited,deals,amount,date) VALUES ('Logan',12,6,7625.0,'2022-02-29');
+INSERT INTO tb_sales(seller_name,visited,deals,amount,date) VALUES ('Logan',12,6,7625.0,'2022-02-20');
 INSERT INTO tb_sales(seller_name,visited,deals,amount,date) VALUES ('Padme',82,82,22465.0,'2022-02-27');
 INSERT INTO tb_sales(seller_name,visited,deals,amount,date) VALUES ('Barry Allen',68,56,12595.0,'2022-02-17');
 INSERT INTO tb_sales(seller_name,visited,deals,amount,date) VALUES ('Logan',27,13,4636.0,'2022-02-16');
