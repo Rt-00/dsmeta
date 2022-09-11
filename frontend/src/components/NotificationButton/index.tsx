8f83136f6111243b15b541a7d5c08af640a21e8c
Rafael Toledo <toledorafael304@gmail.com>
2022-09-11 16:03:46 -0300

    Toast

--- frontend/src/components/NotificationButton/index.tsx
+++ frontend/src/components/NotificationButton/index.tsx
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import { toast } from 'react-toastify';
 import icon from '../../assets/img/notification-icon.svg'
 import { BASE_URL } from '../../utils/request';
 import './styles.css'
@@ -11,8 +12,7 @@ function handleClick(saleId: number) {
 
     axios(`${BASE_URL}/sales/${saleId}/notification`)
     .then(resp => {
-        console.log("Sucesso");
-        
+        toast.info("SMS enviado com sucesso");
     });
 }
 
